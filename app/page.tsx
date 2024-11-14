8c90ccaa455b68a2930f49a8df46b37976e02dee
tranzbook <tranzbook1@gmail.com>
2024-11-14 12:33:04 +0000

    descripy

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent,  CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Dashboard() {
   return (
